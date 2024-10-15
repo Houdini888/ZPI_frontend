c6dfbd4eadba0257d9e3c4d809ff8223ae070722
Jannekk <janis.bo.01@gmail.com>
2024-10-15 17:04:53 +0200

    Test 0.3 library

--- zpi_frontend/lib/main.dart
+++ zpi_frontend/lib/main.dart
@@ -1,10 +1,5 @@
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:zpi_frontend/app_drawer_menu.dart';
-import 'package:zpi_frontend/pdf_document_class.dart';
-import 'package:zpi_frontend/pdf_preview.dart';
-import 'dart:io';
-import 'package:path_provider/path_provider.dart';
 
 
 void main() {
